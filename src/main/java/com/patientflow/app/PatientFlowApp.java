d630fe6bef97cc9ef3698ac727eef59f9f1c9c7e
siphoczulu <siphoczulu@gmail.com>
2025-12-13 20:36:07 +0300

    Add basic PatientFlowApp entry point

---
+++ src/main/java/com/patientflow/app/PatientFlowApp.java
@@ -0,0 +1,8 @@
+package com.patientflow.app;
+
+public class PatientFlowApp {
+    public static void main(String[] args) {
+        System.out.println("=== PatientFlow OS ===");
+        System.out.println("Starting...");
+    }
+}
